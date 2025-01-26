cae37a2860358bd2a63730adf479d11dac97a435
Jamie Huta <118079848+xfearofdarkness@users.noreply.github.com>
2025-01-26 13:04:58 +0100

    Update tileAtlas and use a render texture to draw the tilemap instead of individual tiles

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,43 @@
 bool SortEntityByYPos(const Enemy& e1, const Enemy& e2) {
     return e1.y < e2.y;
 }
+
+RenderTexture2D CreateBackgroundRenderTexture(const std::vector<std::vector<TileType>>& tilemap,Texture2D tileAtlas, int width, int height) {
+    int mapWidth = tilemap[0].size();
+    int mapHeight = tilemap.size();
+
+    RenderTexture2D renderTexture = LoadRenderTexture(width*32, height*32);
+
+    BeginTextureMode(renderTexture);
+    ClearBackground(BLANK);
+
+    for (int y = 0; y < mapHeight; y++) {
+        for (int x = 0; x < mapWidth; x++) {
+            TileType tile = tilemap[y][x];
+
+            Vector2 pos = {
+                static_cast<float>(x * 32),
+                static_cast<float>(y * 32)
+            };
+
+
+            Rectangle srcRect = {
+                static_cast<float>(static_cast<int>(tile) * 32),
+                0.0f,
+                32.0f,
+                32.0f
+            };
+
+            DrawTextureRec(tileAtlas, srcRect, pos, RAYWHITE);
+        }
+    }
+
+    EndTextureMode();
+
+    return renderTexture;
+}
+
+
 int main() {
     InitWindow(480, 360, "2D Adventures Alpha");
     Texture2D text = LoadTexture("../res/icons.png");
@@ -18,21 +55,24 @@ int main() {
         std::cout << "Failed to load texture" << std::endl;
         return 1;
     }
-    SetTextureFilter(text, TEXTURE_FILTER_POINT);
+
     SetTargetFPS(1000);
     Level level;
     std::vector<std::vector<TileType>> tileMap = level.GetTileMap();
     Player player({360, 320}, tileMap);
     std::vector<Enemy> enemies;
+
     for (int i = 0; i < 10; i++) {
         auto enemy = Enemy(static_cast<float>(GetRandomValue(0,32*32)), static_cast<float>(GetRandomValue(0,32*32)));
         enemies.push_back(enemy);
     }
     Camera2D camera = { 0 };
+    RenderTexture2D tilemapTexture = CreateBackgroundRenderTexture(tileMap, text, 32, 32);
+    SetTextureWrap(text, TEXTURE_WRAP_CLAMP);
 
     while (!WindowShouldClose()) {
-        float deltaTime = GetFrameTime();
 
+        float deltaTime = GetFrameTime();
         // Update logic
         player.update(deltaTime);
 
@@ -63,38 +103,17 @@ int main() {
 
         BeginDrawing();
         BeginMode2D(camera);
-        ClearBackground(BLACK);
+        ClearBackground(BLANK);
 
-        const int mapWidth = 32, mapHeight = 32;
-        const int tileSize = level.tile_size();
-        for (int y = 0; y < mapHeight; y++) {
-            for (int x = 0; x < mapWidth; x++) {
-                TileType tileType = tileMap[y][x];
-                Vector2 pos = {
-                    static_cast<float>(x * tileSize),
-                    static_cast<float>(y * tileSize)
-                };
-
-                Rectangle destRect = {
-                    pos.x,
-                    pos.y,
-                    static_cast<float>(tileSize),
-                    static_cast<float>(tileSize)
-                };
-
-                Rectangle srcRect = {
-                    static_cast<float>(static_cast<int>(tileType) * 32),
-                    0.0f,
-                    32.0f,
-                    32.0f
-                };
-                if (static_cast<int>(tileType) == 4) {
-                    DrawTexturePro(text, {0,0,32,32}, destRect, {0, 0}, 0, RAYWHITE);
-                }
-
-                DrawTexturePro(text, srcRect, destRect, {0, 0}, 0, RAYWHITE);
-            }
-        }
+        //Draw tile map
+        DrawTexturePro(
+           tilemapTexture.texture,
+           Rectangle{ 0, 0, (float)tilemapTexture.texture.width, (float)-tilemapTexture.texture.height }, // Flip vertically
+           Rectangle{ 0, 0, (float)tilemapTexture.texture.width, (float)tilemapTexture.texture.height },
+           Vector2{ 0, 0 },
+           0.0f,
+           WHITE
+       );
 
         // Draw player
         DrawTexturePro(
@@ -121,12 +140,13 @@ int main() {
             }
 
         }
-
+        EndTextureMode();
 
         EndMode2D();
         DrawFPS(0,0);
         EndDrawing();
     }
     UnloadTexture(text);
+    UnloadRenderTexture(tilemapTexture);
     CloseWindow();
 }
